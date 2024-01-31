9f57f8f8442a988490bfb27794c43a183cef7056
AigrettaM <maigretta@gmail.com>
2024-01-31 17:55:17 +0700

    Delete file and Dictionary

--- script.js
+++
@@ -1,8 +0,0 @@
-// console.log("Ini adalah console log");
-// console.info("Ini adalah console info");
-// console.warn("ini adalah console warn");
-// console.error("ini adalah console error");
-
-function goToAnotherPage() {
-    window.location.href = 'https://www.upi.edu/';
-}
